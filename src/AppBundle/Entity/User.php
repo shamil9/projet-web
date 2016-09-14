90da5a36490dd4146874b3be202261019f68439c
shamil9 <shamil9@gmail.com>
2016-09-14 11:32:21 +0200

    Intégration initiale de FOSUserBundle

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Entity;
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
