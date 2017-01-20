8960f6d54319fa3138d4cbdad21f2ad553575f94
shamil9 <shamil9@gmail.com>
2017-01-20 19:39:11 +0100

    Done

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,184 +1,170 @@
-'use strict';
+"use strict";
 //Ajout du prestataire dans les favoris
-var addFavorite = function (url, self) {
-    var options = {
-        url: url,
-        method: 'POST',
-        context: self
-    };
+var addFavorite = function(url, self) {
+    var options = { url: url, method: "POST", context: self };
 
-    $.ajax(options)
-        .done(function () {
-            // self
-            //     .attr('id', 'remove-favorite')
-            //     .attr('title', 'supprimer le favori')
-            //     .toggleclass('favorite__active');
-        });
+    $.ajax(options).done(function() {
+        // self
+        //     .attr('id', 'remove-favorite')
+        //     .attr('title', 'supprimer le favori')
+        //     .toggleclass('favorite__active');
+    });
 };
 
 //Suppression du prestataire dans les favoris
-var removeFavorite = function (url, self) {
-    var options = {
-        url: url,
-        method: 'POST',
-        context: self
-    };
+var removeFavorite = function(url, self) {
+    var options = { url: url, method: "POST", context: self };
 
-    $.ajax(options)
-        .done(function () {
-            // self
-            //     .attr('id', 'add-favorite')
-            //     .attr('title', 'Ajouter aux favoris')
-            //     .toggleClass('favorite__active');
-        });
+    $.ajax(options).done(function() {
+        // self
+        //     .attr('id', 'add-favorite')
+        //     .attr('title', 'Ajouter aux favoris')
+        //     .toggleClass('favorite__active');
+    });
 };
 
 //Affichage de la carte GMAPS
-var showGoogleMap = function (address) {
+var showGoogleMap = function(address) {
     var map;
-    map = new GMaps({
-        el: '#map',
-        lat: -12.043333,
-        lng: -77.028333
-    });
+    map = new GMaps({ el: "#map", lat: -12.043333, lng: -77.028333 });
     GMaps.geocode({
         address: address,
-        callback: function (results, status) {
-            if (status == 'OK') {
+        callback: function(results, status) {
+            if (status == "OK") {
                 var latlng = results[0].geometry.location;
 
                 map.setCenter(latlng.lat(), latlng.lng());
-                map.addMarker({
-                    lat: latlng.lat(),
-                    lng: latlng.lng()
-                });
+                map.addMarker({ lat: latlng.lat(), lng: latlng.lng() });
             }
         }
     });
 };
 
 //Affichage des suggestions de prestataire
-var getSuggestions = function (url) {
-
+var getSuggestions = function(url) {
     //Recuperation du json avec les suggestions
-    $.getJSON(url, function (data) {
+    $.getJSON(url, function(data) {
         if (data.length < 1)
-            return showSuggestions('<h3 class="text-center">Aucun résultat trouvé</h3>');
-        var html = '';
+            return showSuggestions(
+                '<h3 class="text-center">Aucun r\xE9sultat trouv\xE9</h3>'
+            );
+        var html = "";
 
         //Construction du HTML
         for (var key in data) {
             html += "<div class='col-lg-3'>" +
-                "<a href='" + data[ key ].slug + "' class='thumbnail'>" +
-                "<img class='img--100x100' src='/assets/img/uploads/avatars/" + data[ key ].picture + "'>" +
-                            "<h4 class='text-center'>" + data[key].name + "</h4>" +
-                        "</a>" +
-                    "</div>";
+                "<a href='" +
+                data[key].slug +
+                "' class='thumbnail'>" +
+                "<img class='img--100x100' src='/assets/img/uploads/avatars/" +
+                data[key].picture +
+                "'>" +
+                "<h4 class='text-center'>" +
+                data[key].name +
+                "</h4>" +
+                "</a>" +
+                "</div>";
         }
 
         function showSuggestions(html) {
-            $('#suggestions').html(html);
+            $("#suggestions").html(html);
         }
 
         showSuggestions(html);
-        }
-    );
+    });
 };
 
 //Enregistrement et affichage d'un nouveau commentaire
-var postComment = function ( params ) {
+var postComment = function(params) {
     var options = {
         url: params.url,
-        method: 'POST',
-        data: $( params.self ).serialize(),
+        method: "POST",
+        data: $(params.self).serialize(),
         context: params.self
     };
 
-    $.ajax(options)
-        .done(function (data) {
-            var comment =
-                "<div class='panel panel-default comment-animate'>" +
-                    "<div class='panel-heading'>" +
-                "<img class='img--30x30 img-circle' src='/assets/img/uploads/avatars/" + params.picture + "'/>" +
-                "<span class='lead' style='vertical-align: middle;'>" + params.user + "</span>" +
-                        "<span class='pull-right rating' style='width: calc(20px * " + data.rating + ");'></span>" +
-                    "</div>" +
-                    "<div class='panel-body'>" +
-                        data.comment +
-                    "</div>" +
-                "</div>";
-            $(self)
-                .find('.btn')
-                .html('')
-                .attr('disabled', 'true')
-                .addClass('glyphicon glyphicon-ok')
-                .end()
-                .find('#comment_comment')
-                .attr('disabled', 'true');
+    $.ajax(options).done(function(data) {
+        var comment = "<div class='panel panel-default comment-animate'>" +
+            "<div class='panel-heading'>" +
+            "<img class='img--30x30 img-circle' src='/assets/img/uploads/avatars/" +
+            params.picture +
+            "'/>" +
+            "<span class='lead' style='vertical-align: middle;'>" +
+            params.user +
+            "</span>" +
+            "<span class='pull-right rating' style='width: calc(20px * " +
+            data.rating +
+            ");'></span>" +
+            "</div>" +
+            "<div class='panel-body'>" +
+            data.comment +
+            "</div>" +
+            "</div>";
+        $(self)
+            .find(".btn")
+            .html("")
+            .attr("disabled", "true")
+            .addClass("glyphicon glyphicon-ok")
+            .end()
+            .find("#comment_comment")
+            .attr("disabled", "true");
 
-            $(comment).prependTo('#comments-list');
-        });
+        $(comment).prependTo("#comments-list");
+    });
 };
 
 // Inscription à la newsletter
-var addNewsletterSubscriber = function (url, user, self) {
-    $(self).on('click', function (e) {
+var addNewsletterSubscriber = function(url, user, self) {
+    $(self).on("click", function(e) {
         e.preventDefault();
 
-        var options = {
-            url: url,
-            method: 'POST',
-            context: self
-        };
+        var options = { url: url, method: "POST", context: self };
 
-        $.ajax(options)
-            .done(function () {
-                $(this)
-                    .removeAttr('id')
-                    .attr('id', 'unsubscribe')
-                    .attr('disabled', 'true')
-                    .toggleClass('btn-success')
-                    .html('Succès');
-            });
+        $.ajax(options).done(function() {
+            $(this)
+                .removeAttr("id")
+                .attr("id", "unsubscribe")
+                .attr("disabled", "true")
+                .toggleClass("btn-success")
+                .html("Succ\xE8s");
+        });
     });
 };
 
 // Désinscription de la newsletter
-var removeNewsletterSubscriber = function (url, user, self) {
-    $(self).on('click', function (e) {
+var removeNewsletterSubscriber = function(url, user, self) {
+    $(self).on("click", function(e) {
         e.preventDefault();
 
-        var options = {
-            url: url,
-            method: 'POST',
-            context: self
-        };
+        var options = { url: url, method: "POST", context: self };
 
-        $.ajax(options)
-            .done(function () {
-                $(this)
-                    .removeAttr('id')
-                    .attr('id', 'unsubscribe')
-                    .attr('disabled', 'true')
-                    .toggleClass('btn-success')
-                    .html('Succès');
-            });
+        $.ajax(options).done(function() {
+            $(this)
+                .removeAttr("id")
+                .attr("id", "unsubscribe")
+                .attr("disabled", "true")
+                .toggleClass("btn-success")
+                .html("Succ\xE8s");
+        });
     });
 };
 
 //Signaler un commentaire
 var reportComment = function(url, id) {
-    var self = $('#comment-report-' + id);
-    self.on('submit', function(e) {
-    e.preventDefault();
+    var self = $("#comment-report-" + id);
+    self.on("submit", function(e) {
+        e.preventDefault();
 
-    $.ajax({
-        url: url,
-        method: 'post',
-        data: self.serialize(),
-        context: self
-    }).done(function () {
-            $('#reportComment-' + id).modal('hide');
-        });
+        $
+            .ajax({
+                url: url,
+                method: "post",
+                data: self.serialize(),
+                context: self
+            })
+            .done(function() {
+                $("#reportComment-" + id).modal("hide");
+            });
     });
 };
+
